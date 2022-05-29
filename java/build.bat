c75320ab9a68ba8fa7554f1e6b8a46151ad4d956
sjmdev <sjorsmallon@gmail.com>
2022-05-29 15:36:54 +0200

    [feat] astprinter

--- java/build.bat
+++ java/build.bat
@@ -1,1 +1,4 @@
 echo C:\Users\Sjors\Desktop\jdk\jdk-18.0.1.1\bin\java -cp C:\Users\Sjors\Desktop\crafting_interpreters\java com.craftinginterpreters.lox.Lox
+
+
+echo C:\Users\Sjors\Desktop\jdk\jdk-18.0.1.1\bin\java -cp C:\Users\Sjors\Desktop\crafting_interpreters\java com.craftinginterpreters.tool.GenerateAst
